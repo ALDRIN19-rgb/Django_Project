532ab4f2366bac9e574d48a76f9ab94952f982a0
ALDRIN19-rgb <caseresaldrin01@gmail.com>
2026-02-17 07:43:04 +0800

    Add files via upload

---
+++ caseres/myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Note  # import your Task model 
+
+# Register the model
+admin.site.register(Note)
+
+# Register your models here.
---
+++ caseres/myapp/views.py
@@ -0,0 +1,19 @@
+import requests
+from django.shortcuts import render
+
+def home(request):
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    data = response.json()
+
+    context = {
+        "posts": data
+    }
+
+    return render(request, 'myapp/home.html', context)
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
